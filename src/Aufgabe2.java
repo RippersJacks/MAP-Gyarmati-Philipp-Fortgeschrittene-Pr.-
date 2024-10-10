54d075ab9297bc25999a0c6144084bdaeffb5dba
Philipp-Teodor Gyarmati <philipp.gyarmati@gmail.com>
2024-10-10 11:25:16 +0300

    Aufgabe 2 fertiggestellt

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -1,2 +1,51 @@
+import java.util.Arrays;
+
 public class Aufgabe2 {
+    int n;
+    int[] array = new int[n];
+
+    Aufgabe2(int[] arrayParam){
+        n = arrayParam.length;
+        array = arrayParam;
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    int findeMaxi(){
+        int maxi = 0;
+        for (int i=0;i<n;i++)
+            if (array[i] > maxi)
+                maxi = array[i];
+        return maxi;
+    }
+
+    int findeMini(){
+        int mini = 50000;
+        for (int i=0;i<n;i++)
+            if (array[i] < mini)
+                mini = array[i];
+        return mini;
+    }
+
+    int maximaleSumme(){
+        int mini = 50000, summe = 0;
+        for (int i=0;i<n;i++){
+            summe += array[i];
+            if (array[i] < mini)
+                mini = array[i];
+        }
+        summe -= mini;
+        return summe;
+    }
+
+    int minimaleSumme(){
+        int maxi = 0, summe = 0;
+        for (int i=0;i<n;i++){
+            summe += array[i];
+            if (array[i] > maxi)
+                maxi = array[i];
+        }
+        summe -= maxi;
+        return summe;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
 
+        System.out.println("Aufgabe 1");
+
         //Aufgabe 1
         Aufgabe1 university = new Aufgabe1();
         int[] arrayNoten = new int[]{50, 47, 69, 30, 17, 81, 73};
@@ -17,5 +19,19 @@ public class Main {
         System.out.println("Grosste aufgerundete Note: " + university.Methode4(arrayNoten));
 
 
+        System.out.println("\nAufgabe 2");
+
+        //Aufgabe 2
+        int[] arrayParam = new int[]{4,8,3,10,17};
+        Aufgabe2 arrayOfNumbers = new Aufgabe2(arrayParam);
+
+        //Methode1
+        System.out.println("Maximalzahl: " + arrayOfNumbers.findeMaxi());
+        //Methode2
+        System.out.println("Minimalzahl: " + arrayOfNumbers.findeMini());
+        //Methode3
+        System.out.println("Maximale Summe von n-1 Zahlen: " + arrayOfNumbers.maximaleSumme());
+        //Methode4
+        System.out.println("Minimale Summe von n-1 Zahlen: " + arrayOfNumbers.minimaleSumme());
     }
 }
